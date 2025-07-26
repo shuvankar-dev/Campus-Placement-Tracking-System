86c30996054f7e9bd0c248e4758e89a2d103fa55
Shuvankar-dev <shuvankardas.2910@gmail.com>
2025-07-27 00:17:11 +0530

    implement custom css

--- tpo/register.php
+++ tpo/register.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TPO Registration</title>
+    <link rel="stylesheet" href="../assets/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
 </head>
 <body class="bg-light">
